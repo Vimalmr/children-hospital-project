dd37e07e72f9acf830456e803f5ab3f5d78b49f1
Ragavi-Ra <ragaviramasamyrr@gmail.com>
2024-06-15 12:07:26 +0530

    added css to sidebar and simple route

---
+++ client/src/components/Patient Reports/PatientReports.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style7 from './PatientReports.module.css'
+
+const PatientReports = () => {
+  return (
+    <div>
+      <div className={style7.container}>
+        <button>Report Details</button>
+        <button>Report Entry</button>
+      </div>
+    </div>
+  )
+}
+
+export default PatientReports
