41da558b3c4d49957199d5d4ddd7f5fde7dc610c
Vimalmr <vimalmr65@gmail.com>
2024-09-08 12:16:10 +0530

    Backend - Patient Details Display

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -13,15 +13,16 @@ router.post('/', async (req, res) => {
 		console.log(patientinfo);
 		console.log(motherinfo);
 		console.log(fatherinfo);
-		console.log(patientinfo.admno);
+		console.log(fatherinfo.habits.smoke);
 		const [patient] = await pool.query(`SELECT * FROM patientinfo WHERE AdmissionNumber='${patientinfo.admno}';`);
 		if (Object.keys(patient).length !== 0)
 	        return res.status(409).send({ message: "User with given userid already Exist!" });
 	    
-        // const [query] = await pool.query(`INSERT INTO logininfo VALUES ('${req.body.userid}','${hashPassword}','${req.body.role}');`);
         const [query1] = await pool.query(`INSERT INTO patientinfo VALUES ('${patientinfo.admno}', '${patientinfo.name}', '${patientinfo.dob}', '${patientinfo.age}', '${patientinfo.address}','${patientinfo.pin}', '${patientinfo.insno}', '${patientinfo.aadharid}', NOW());`);
-
-		res.status(201).send({ message: "Patient Entry successfull" });
+		const [query2] = await pool.query(`INSERT INTO motherinfo(AdmissionNumber, Mother_Name, Mother_Age, Mother_Occupation, Mother_Health_General, Mother_Diabetes, Mother_Illness, Mother_Drugs, Mother_Exposure_Teratogens) VALUES ('${patientinfo.admno}', '${motherinfo.name}', '${motherinfo.age}', '${motherinfo.moocc}', '${motherinfo.hegnrl}', '${motherinfo.modiab}', '${motherinfo.moill}', '${motherinfo.modrg}', '${motherinfo.moexts}');`);
+		const [query3] = await pool.query(`INSERT INTO fatherinfo(AdmissionNumber, Father_Name, Father_Age, Father_Occupation, Father_Medication, Father_Past_illness, Father_Smoke, Father_Alcohol, Father_Tobacco) VALUES ('${patientinfo.admno}', '${fatherinfo.name}', '${fatherinfo.age}', '${fatherinfo.faocc}', '${fatherinfo.famed}', '${fatherinfo.fapaill}', '${fatherinfo.habits.smoke}', '${fatherinfo.habits.tobacco}', '${fatherinfo.habits.alcohol}');`);
+		console.log(query3);
+		res.status(201).send({ message: "Patient, Mother Entry successfull" });
 
 	} catch (error) {
     console.log(error);
