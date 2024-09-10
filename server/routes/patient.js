105d9afbd2117153e0d19f967c8ff6ead54c958a
Vimalmr <vimalmr65@gmail.com>
2024-09-10 23:09:55 +0530

    patient.js error fix

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../db');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 router.post('/', async (req, res) => {
 
